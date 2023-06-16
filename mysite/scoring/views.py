46953ee80aa9a2c36910d061397dd58ba530305f
Daniel Caballero <daniel.caballero@klarna.com>
2023-06-16 14:45:04 +0200

    Capture query string parameter

--- mysite/scoring/views.py
+++ mysite/scoring/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpRequest, HttpResponse
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the scoring app index.")
+def index(request: HttpRequest):
+    return HttpResponse(f"Hello, world. You're at the scoring app index for game: {request.GET.get('game')}")
