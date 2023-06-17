f6e6c410bee1c671c1a8c725e7ed7ddfae42d343
Daniel Caballero <daniel.caballero@klarna.com>
2023-06-17 22:45:36 +0200

    Add redirect to save method on form post

--- mysite/scoring/urls.py
+++ mysite/scoring/urls.py
@@ -6,5 +6,5 @@ app_name = "scoring"
 urlpatterns = [
     path("", views.index, name="index"),
     path("score/<str:game>/", views.game, name="game"),
-    path("save/", views.save, name="save")
+    path("save/<str:game>/", views.save, name="save")
 ]
--- mysite/scoring/views.py
+++ mysite/scoring/views.py
@@ -1,5 +1,6 @@
-from django.http import HttpRequest, HttpResponse
+from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
 from django.template import loader
+from django.urls import reverse
 
 def index(request: HttpRequest):
     template = loader.get_template("scoring/index.html")
@@ -9,9 +10,11 @@ def index(request: HttpRequest):
 
 def game(request: HttpRequest, game: str):
     template = loader.get_template(f"scoring/{game}.html")
-    context = {}
+    context = {
+        "game": game,
+    }
     return HttpResponse(template.render(context, request))
 
 
-def save(request: HttpRequest):
-    return HttpResponse(f"{request.POST.get('score-21')}")
+def save(request: HttpRequest, game: str):
+    return HttpResponseRedirect(reverse("scoring:game", args=(game,)))
