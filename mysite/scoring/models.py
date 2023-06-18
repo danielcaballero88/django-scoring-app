00a6589f220c9c23434aae7a68dfdedc97d57b9f
Daniel Caballero <daniel.caballero@klarna.com>
2023-06-18 10:14:39 +0200

    Add models to scoring app

--- mysite/scoring/models.py
+++ mysite/scoring/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+class Player(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Game(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class ScoringCategory(models.Model):
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+
+
+class Board(models.Model):
+    game = models.ForeignKey(Game, on_delete=models.RESTRICT)
+
+
+class BoardPlayer(models.Model):
+    board = models.ForeignKey(Board, on_delete=models.CASCADE)
+    player = models.ForeignKey(Player, on_delete=models.RESTRICT)
+
+
+class Score(models.Model):
+    player = models.ForeignKey(Player, on_delete=models.RESTRICT)
+    board = models.ForeignKey(Board, on_delete=models.CASCADE)
+    scoring_category = models.ForeignKey(ScoringCategory, on_delete=models.RESTRICT)
+    value = models.IntegerField(default=0)
