44e072449904530337ebe25a6940d8866cc1edbd
Daniel Caballero <daniel.caballero@klarna.com>
2023-06-23 17:29:30 +0200

    First auth login implementation

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,37 @@
+from django.contrib import messages
+from django.contrib.auth import authenticate
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
-# Create your views here.
+from .forms import LoginForm
+
+
+def login(request):
+    # if this is a POST request we need to process the form data
+    if request.method == "POST":
+        # create a form instance and populate it with data from the request:
+        form = LoginForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password"]
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                # A backend authenticated the credentials
+                messages.success(request, f"Login success for {username}")
+            else:
+                # No backend authenticated the credentials
+                messages.error(request, f"Login error for {username}")
+            # redirect to a new URL:
+            return HttpResponseRedirect(reverse("scoring:index"))
+        # else: the render statement at the end of this view will render
+        # the form again showing the errors that should be attached to
+        # the form in the POST request (?... need to check of course)
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = LoginForm()
+
+    return render(request, "accounts/login.html", {"form": form})
--- mysite/settings.py
+++ mysite/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'scoring.apps.ScoringConfig',
+    'accounts.apps.AccountsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
