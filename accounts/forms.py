15a8b9f1507f1b7d50b71afd990304b28e61d407
Daniel Caballero <daniel.caballero@klarna.com>
2023-06-23 17:39:32 +0200

    Hide password input using PasswordInput widget

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,4 +3,4 @@ from django import forms
 
 class LoginForm(forms.Form):
     username = forms.CharField(label="Username", max_length=100)
-    password = forms.CharField(label="Password", max_length=50)
+    password = forms.CharField(label="Password", max_length=50, widget=forms.PasswordInput)
