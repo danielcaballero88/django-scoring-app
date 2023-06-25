c15f284da67750c8d0df840d0f9eb57787a06502
Daniel Caballero <daniel.caballero@klarna.com>
2023-06-25 22:24:10 +0200

    Add logic to register users

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,7 @@
 from django.contrib import messages
 from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
+from django.db.utils import IntegrityError
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
@@ -45,9 +47,32 @@ def register(request):
         # check whether it's valid:
         if form.is_valid():
             # process the data in form.cleaned_data as required
-            ...
-            # redirect to a new URL:
-            return HttpResponseRedirect(reverse("scoring:index"))
+            username = form.cleaned_data["username"]
+            email = form.cleaned_data["email"]
+            password = form.cleaned_data["password"]
+            try:
+                user = User.objects.create_user(
+                    username=username,
+                    email=email,
+                    password=password,
+                )
+                user.save()
+            except IntegrityError as exc:
+                if (
+                    hasattr(exc, "args")
+                    and isinstance(exc.args, tuple)
+                    and isinstance(exc.args[0], str)
+                    and "UNIQUE" in exc.args[0]
+                ):
+                    messages.error(
+                        request, f"Cannot register user {username}, alrady exists."
+                    )
+                else:
+                    raise exc
+            else:
+                messages.success(request, f"User {username} was registered correctly.")
+                # redirect to a new URL:
+                return HttpResponseRedirect(reverse("scoring:index"))
         # else: the render statement at the end of this view will render
         # the form again showing the errors that should be attached to
         # the form in the POST request (?... need to check of course)
