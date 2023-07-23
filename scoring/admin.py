82f11ddd2cf42f81a920e527ed252f202f949103
Daniel Caballero <daniel.caballero@klarna.com>
2023-07-23 18:34:27 +0200

    Add scoring models to admin site

--- scoring/admin.py
+++ scoring/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Game, Player, ScoringCategory
+from .models import Game, Player, ScoringCategory, Board, Scorer
 
 
 
@@ -14,10 +14,26 @@ class GameAdmin(admin.ModelAdmin):
     inlines= [ScoringCategoryInLine]
 
 
+class BoardInLine(admin.TabularInline):
+    model = Board
+    extra = 1
+
+
+class ScorerInLine(admin.TabularInline):
+    model = Scorer
+    extra = 3
+
+
+class BoardAdmin(admin.ModelAdmin):
+    inlines = [ScorerInLine]
+
+
 class PlayerAdmin(admin.ModelAdmin):
-    ...
+    inlines = [BoardInLine]
 
 
 admin.site.register(Game, GameAdmin)
 
 admin.site.register(Player, PlayerAdmin)
+
+admin.site.register(Board, BoardAdmin)
